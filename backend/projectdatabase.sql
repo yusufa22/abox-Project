972c2c4d022e1670a368a5bee2fca73d3eb74b0c
ayub@AyubHP <ayubabdikarim@protonmail.com>
2024-03-24 09:24:37 +0000

    cleanup

--- backend/projectdatabase.sql
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-CREATE TABLE  users (id BIGSERIAL PRIMARY KEY, name TEXT, email TEXT UNIQUE, password TEXT, status TEXT, reset TEXT)
-CREATE TABLE files (owner BIGINT REFERENCES users (id), name TEXT, type TEXT, mtime TEXT, folder TEXT, shared_by TEXT, shared_with TEXT)
-=======
-CREATE TABLE users (id BIGSERIAL PRIMARY KEY, name TEXT, email TEXT UNIQUE, password TEXT, status TEXT, reset TEXT)
-CREATE TABLE files (owner REFERENCES users(id), name TEXT, type TEXT, mtime TEXT, folder TEXT, shared_with TEXT, shared_by TEXT)
->>>>>>> a5ab2df (added message to homepage)
