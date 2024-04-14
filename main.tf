484a0ea132011696e7a380abd4e3944bcc403882
ayub@AyubHP <ayubabdikarim@protonmail.com>
2024-04-14 18:25:10 +0100

    quick fix

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_policy" "function_logging_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
-  role = aws_iam_role.lambda_role.arn
+  role = aws_iam_role.lambda_role.id
   policy_arn = aws_iam_policy.function_logging_policy.arn
 }
 
